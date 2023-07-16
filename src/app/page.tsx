013d7e46d81fb1b346e763a55a56d22d8d472670
Hiresh <hbremanand4@gmail.com>
2023-07-16 22:10:13 +0100

    link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
                 <a target='_blank' href="https://anseljobs.com/" className="inline-flex w-fit items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     View
                     <svg className="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
                 </a>
             </div>
@@ -56,7 +56,7 @@ export default function Home() {
                 <a target='_blank' href="https://www.anselimmigration.com/" className="inline-flex justify-end w-fit items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     View
                     <svg className="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
                 </a>
             </div>
@@ -68,7 +68,7 @@ export default function Home() {
                 <a target='_blank' href="https://github.com/hireshBrem" className="inline-flex w-fit justify-end items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     View
                     <svg className="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
                 </a>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,16 +6,20 @@ import { useState } from "react";
 export default function Navbar() {
     const [navbarOpen, setNavbarOpen] = useState(false)
 
+    function scrollToSection(id: string) {
+        document.getElementById(id)?.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"}) 
+    }
+
     return(
 <nav className="bg-gray-900 font-lato sticky w-full z-20 top-0 left-0">
   <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
     <div className="items-center hidden w-full md:inline-flex">
         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 text-lg">
-            <Link href="#" className="block md:hover:text-sky-500 py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0">About</Link>
-            <Link href="#ideas" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Ideas</Link>
-            <Link href="#projects" className="block md:hover:text-sky-500 py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0">Projects</Link>
-            <Link href="#services" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Services</Link>
-            <Link href="#testimonials" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Testimonials</Link>
+            <Link href="/#" className="/block md:hover:text-sky-500 py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0">About</Link>
+            <Link href="/#ideas" className="/block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Ideas</Link>
+            <Link href="/#projects" className="/block md:hover:text-sky-500 py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0">Projects</Link>
+            <Link href="/#services" className="/block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Services</Link>
+            <Link href="/#testimonials" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Testimonials</Link>
         </ul>
     </div>
     <div className="flex justify-between">
@@ -32,11 +36,11 @@ export default function Navbar() {
         navbarOpen ===true ? 
         <div className="absolute md:hidden rounded-r-md w-[40%]  bg-gradient-to-tr from-indigo-500 from-10 to-sky-500">
             <ul>
-                <Link href="#about" className="block py-2 pl-3 pr-4 text-white rounded ">About</Link>
-                <Link href="#ideas" className="block py-2 pl-3 pr-4 rounded text-white">Ideas</Link>
-                <Link href="#projects" className="block py-2 pl-3 pr-4 rounded text-white">Projects</Link>
-                <Link href="#services" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Services</Link>
-                <Link href="#testimonials" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Testimonials</Link>
+                <Link href="/#about" className="block py-2 pl-3 pr-4 text-white rounded ">About</Link>
+                <Link href="/#ideas" className="block py-2 pl-3 pr-4 rounded text-white">Ideas</Link>
+                <Link href="/#projects" className="block py-2 pl-3 pr-4 rounded text-white">Projects</Link>
+                <Link href="/#services" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Services</Link>
+                <Link href="/#testimonials" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-500 md:p-0 text-white">Testimonials</Link>
             </ul>
 
         </div>
