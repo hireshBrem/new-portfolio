e13c5c7ee36bba19a84e5099fb5a5fcb82119f7b
Hiresh <hbremanand4@gmail.com>
2023-08-16 15:18:50 +0100

    booklab update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,9 @@ export default function Home() {
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Ideas</h2>
                 <p className="mb-8 font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">I have an idea, and I build it!</p>
             </div>
-            <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <a target='_blank' href="https://booklabbeta.vercel.app" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">BookLab (Coming soon)</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">BookLab</h5>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Collaborative book reading platform, where you could read and write collaboratively. Ask questions and explore topics like never before.</p>
                 </div>
             </a>
